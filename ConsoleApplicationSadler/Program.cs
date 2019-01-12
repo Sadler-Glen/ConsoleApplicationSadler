d87ebd4cd341b2ce5c0ede766904c8de4038e979
Glen Sadler <31795400+Sadler-Glen@users.noreply.github.com>
2019-01-13 09:04:13 +1300

    Add GlobaVar Class
    Added GlobalVar Class to allow a global variable lines to be used

--- ConsoleApplicationSadler/Program.cs
+++ ConsoleApplicationSadler/Program.cs
@@ -7,6 +7,8 @@
 
 namespace ConsoleApplicationSadler
 {
+
+
     class Program
     {
         static void Main(string[] args)
@@ -27,10 +29,9 @@ namespace ConsoleApplicationSadler
             
             fullName = "Glen Sadler";
             location = "New Zealand";
-
-            Console.WriteLine("=====================================================================");
+            Console.WriteLine(GlobalVar.lines);
             Console.WriteLine(" M Y   I N T R O D U C T I O N");
-            Console.WriteLine("=====================================================================\r\n");
+            Console.WriteLine($"{GlobalVar.lines} \r\n");
             // use String Interpolation as required for this assignement
             Console.WriteLine($" My name is {fullName}.");
             Console.WriteLine($" I am from {location}.\r\n");
@@ -49,9 +50,9 @@ namespace ConsoleApplicationSadler
             DateTime christmas = new DateTime(christmasYear, 12, 25);
             TimeSpan numberOfDays = christmas - today;
 
-            Console.WriteLine("=====================================================================");
+            Console.WriteLine(GlobalVar.lines);
             Console.WriteLine(" D A T E   C A L C U L A T I O N S");
-            Console.WriteLine("=====================================================================\r\n");
+            Console.WriteLine($"{GlobalVar.lines} \r\n");
             Console.WriteLine($" The date today is {today.ToString("d")} (NZ Region d/mm/yyyy).\r\n");
             Console.WriteLine($" There are {numberOfDays.TotalDays} days to Christmas {christmas.Year}.\r\n");            
         }
@@ -61,9 +62,9 @@ namespace ConsoleApplicationSadler
             double width, height, woodLength, glassArea;
             string widthString, heightString;
 
-            Console.WriteLine("=====================================================================");
+            Console.WriteLine(GlobalVar.lines);
             Console.WriteLine(" G L A Z I N G  C A L C U L A T O R");
-            Console.WriteLine("=====================================================================\r\n");
+            Console.WriteLine($"{GlobalVar.lines} \r\n");
 
             while (true)
             {
@@ -123,10 +124,16 @@ namespace ConsoleApplicationSadler
 
         private static void ClearConsole()          // Added to clear console after each method has run
         {
-            Console.WriteLine("=====================================================================\r\n");
+            Console.WriteLine($"{GlobalVar.lines} \r\n");
             Console.Write(" Press any key to continue...");
             Console.ReadKey(true);
             Console.Clear();
         }                     
     }
+
+    public static class GlobalVar // added global variable message
+    {
+        public const string lines = "=====================================================================";
+    }
+
 }
